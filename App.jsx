7fe82d3fd231b748b0e21e2959a7d1b0be9110dd
Kunj Patel <pkunjk007@gmail.com>
2025-11-12 12:19:57 -0700

    Cot the basic navigation setup

--- App.jsx
+++ App.jsx
@@ -5,38 +5,68 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
 import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+  Image,
+  Text,
+  View,
+} from 'react-native';
 import './global.css'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import ToDoScreen from './src/screens/ToDoScreen';
+import CalendarScreen from './src/screens/CalendarScreen';
+import JournalScreen from './src/screens/JournalScreen';
+import {NavigationContainer} from '@react-navigation/native';
+
+const Tab=createBottomTabNavigator();
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
+    <NavigationContainer>
+      <Tab.Navigator 
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            // Map route names to image sources
+            const iconMap = {
+              ToDo: require('./src/assets/images/check-box.png'),
+              Calendar: require('./src/assets/images/today.png'),
+              Journal: require('./src/assets/images/edit.png'),
+            };
+
+            return (
+              <View className={` p-2 rounded-full ${focused ? 'bg-[#D6DDE3]' : 'bg-transparent'}`}>
+                <Image 
+                source={iconMap[route.name]} 
+                style={{
+                  width: 25,
+                  height: 25,
+                  tintColor: color, // Automatically changes color
+                }}
+                />
+              </View>
+              
+            );
+          },
+          tabBarActiveTintColor: '#000000',    // Black when active
+          tabBarInactiveTintColor: '#8E8E93',  // Gray when inactive
+          tabBarStyle:{paddingTop:7, backgroundColor: '#EAEEF1'},
+          tabBarLabelStyle:{marginTop: 5, fontSize: 13},
+        })}>
+        <Tab.Screen 
+          name="ToDo" 
+          component={ToDoScreen} 
+        />
+        <Tab.Screen
+          name="Calendar"
+          component={CalendarScreen}
+        />
+        <Tab.Screen
+          name="Journal"
+          component={JournalScreen}  
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
+};
 
 export default App;
---
+++ src/screens/ToDoScreen.jsx
@@ -0,0 +1,36 @@
+import {
+  Button,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+export default function ToDoScreen() {
+
+  return (
+    <View >
+      <Button
+        title="Go to About"
+      />
+      <Button
+        title="Go to Counter"
+      />
+      <Button
+        title="Dog Adoption"
+      />
+      <ScrollView>
+        <Text >Hello World!</Text>
+        <Text >
+          This is my first React Native application!
+        </Text>
+        <Image
+          source={{
+            uri: 'https://images.dog.ceo/breeds/sheepdog-indian/Himalayan_Sheepdog.jpg',
+          }}
+        />
+      </ScrollView>
+    </View>
+  );
+}
