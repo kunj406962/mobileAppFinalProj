3f6eb5e5b01a197b9e62b5819ef7faf27199db66
Kunj Patel <pkunjk007@gmail.com>
2025-11-26 15:49:22 -0700

    Finished stuff

--- src/assets/components/PopUp.jsx
+++ src/assets/components/PopUp.jsx
@@ -64,7 +64,6 @@ const PopUp = ({ visible, onClose, onAddTodo, time, onTimeChange }) => {
                   display="spinner"
                   onChange={handleTimeChange}
                   is24Hour={true}
-                  minimumDate={new Date()}
                 />
               </View>
             )}
